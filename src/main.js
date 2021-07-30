23303fe9f5d6de7f61a0d68c3758af8a488f5309
Muhammad Owais <m.owais2118@gmail.com>
2021-07-30 18:43:48 +0500

    compleyed app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({ store, render: (h) => h(App) }).$mount("#app");
